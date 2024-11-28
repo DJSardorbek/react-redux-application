d0671dcfe6c20f81af03dbf2971845f558537cb1
DJSardorbek <githubsardorbek65119@gmail.com>
2024-11-28 23:47:29 +0500

    #8. Navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import {Main, Login, Register} from './components'
+import {Navbar} from './components'
 
 function App() {
   return (
     <div className="App">
+        <Navbar/>
         <Routes>
           <Route path={'/'} element={<Main/>}/>
           <Route path={'/login'} element={<Login/>}/>
