d0a186a780bec29f9bcea93f08b7b8e2d5253aae
DJSardorbek <githubsardorbek65119@gmail.com>
2024-11-28 23:30:40 +0500

    #7. Full Stack loyiha start

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import {Main, Login, Register} from './components'
+
+function App() {
+  return (
+    <div className="App">
+        <Routes>
+          <Route path={'/'} element={<Main/>}/>
+          <Route path={'/login'} element={<Login/>}/>
+          <Route path={'/register'} element={<Register/>}/>
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
